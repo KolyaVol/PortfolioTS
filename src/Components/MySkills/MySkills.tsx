648a983dff2de0a136fbacaeee2a07686472da81
VolNa <grizli887@mail.ru>
2023-01-08 17:55:20 +0300

    wrong progress bars

--- src/Components/MySkills/MySkills.tsx
+++ src/Components/MySkills/MySkills.tsx
@@ -1,22 +1,62 @@
-import SkillProgressBar from '../UI/SkillProgressBar/SkillProgressBar';
-import './MySkills.css'
+import "./MySkills.css";
 
 export default function MySkills() {
-	
 	return (
 		<ul className="skills">
 			<li>
 				HTML5
-				<SkillProgressBar/>
+				<svg className="svg" xmlns="http://www.w3.org/2000/svg">
+					<circle className={"html5 disactive"} cx={"50%"} cy={"50%"} r={"2rem"} />
+				</svg>
 			</li>
-			<li>CSS3</li>
-			<li>Responsive design</li>
-			<li>Git</li>
-			<li>JavaScript</li>
-			<li>npm</li>
-			<li>NodeJS</li>
-			<li>ReactJS</li>
-			<li>TypeScript</li>
+			<li>
+				CSS3
+				<svg className="svg" xmlns="http://www.w3.org/2000/svg">
+					<circle className={"css3"} cx={"50%"} cy={"50%"} r={"2rem"} />
+				</svg>
+			</li>
+			<li>
+				Responsive design
+				<svg className="svg" xmlns="http://www.w3.org/2000/svg">
+					<circle className={"Responsive"} cx={"50%"} cy={"50%"} r={"2rem"} />
+				</svg>
+			</li>
+			<li>
+				Git
+				<svg className="svg" xmlns="http://www.w3.org/2000/svg">
+					<circle className={"Git"} cx={"50%"} cy={"50%"} r={"2rem"} />
+				</svg>
+			</li>
+			<li>
+				JavaScript
+				<svg className="svg" xmlns="http://www.w3.org/2000/svg">
+					<circle className={"JavaScript"} cx={"50%"} cy={"50%"} r={"2rem"} />
+				</svg>
+			</li>
+			<li>
+				npm
+				<svg className="svg" xmlns="http://www.w3.org/2000/svg">
+					<circle className={"npm"} cx={"50%"} cy={"50%"} r={"2rem"} />
+				</svg>
+			</li>
+			<li>
+				NodeJS
+				<svg className="svg" xmlns="http://www.w3.org/2000/svg">
+					<circle className={"NodeJS"} cx={"50%"} cy={"50%"} r={"2rem"} />
+				</svg>
+			</li>
+			<li>
+				ReactJS
+				<svg className="svg" xmlns="http://www.w3.org/2000/svg">
+					<circle className={"ReactJS"} cx={"50%"} cy={"50%"} r={"2rem"} />
+				</svg>
+			</li>
+			<li>
+				TypeScript
+				<svg className="svg" xmlns="http://www.w3.org/2000/svg">
+					<circle className={"TypeScript"} cx={"50%"} cy={"50%"} r={"2rem"} />
+				</svg>
+			</li>
 		</ul>
 	);
 }
--- src/Components/UI/SkillProgressBar/SkillProgressBar.tsx
+++ src/Components/UI/SkillProgressBar/SkillProgressBar.tsx
@@ -1,46 +1,10 @@
-import React from "react";
+import "./SkillProgressBar.css";
 
 export default function SkillProgressBar() {
+	
 	return (
 		<div className="frame">
-			<div>
-				<div className="headline">
-					<div>your daily progress</div>
-				</div>
-				<div className="circle-big">
-					<div className="text">
-						2758<div className="small">kcal</div>
-					</div>
-					<svg>
-						<circle className="bg" cx="57" cy="57" r="52"></circle>
-						<circle className="progress" cx="57" cy="57" r="52"></circle>
-					</svg>
-				</div>
-				<div className="circles-small">
-					<div className="circle-small">
-						<div className="text">
-							8563
-							<br />
-							<span className="small">steps</span>
-						</div>
-						<svg>
-							<circle className="bg" cx="40" cy="40" r="37"></circle>
-							<circle className="progress one" cx="40" cy="40" r="37"></circle>
-						</svg>
-					</div>
-					<div className="circle-small">
-						<div className="text">
-							6,2
-							<br />
-							<span className="small">km</span>
-						</div>
-						<svg>
-							<circle className="bg" cx="40" cy="40" r="37"></circle>
-							<circle className="progress two" cx="40" cy="40" r="37"></circle>
-						</svg>
-					</div>
-				</div>
-			</div>
+			
 		</div>
 	);
 }
